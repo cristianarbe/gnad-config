22b8bb74556d89eb045afb8f63045706c3ca7be1
cristianarbe <c.ariza@posteo.net>
2019-12-11 17:33:37 +0000

    Add shellcheck disables

--- modules/dotfiles.sh
+++ modules/dotfiles.sh
@@ -1,9 +1,12 @@
-#!/bin/bash
+#!/bin/env bash
 DOT_FILES="https://github.com/cristianarbe/dotfiles.git"
 
-echo 'alias dotfiles="/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME"' >> $HOME/.bashrc
-source ~/.bashrc
-echo ".dotfiles" >> .gitignore
-git clone --bare $DOT_FILES $HOME/.dotfiles
+# shellcheck disable=SC2016
+echo 'alias dotfiles="/usr/bin/git --git-dir=$HOME/.dotfiles/ 
+--work-tree=$HOME"' >>~/.bashrc
+# shellcheck disable=SC1090
+. ~/.bashrc
+echo ".dotfiles" >>.gitignore
+git clone --bare $DOT_FILES ~/.dotfiles
 dotfiles reset --hard
 dotfiles config --local status.showUntrackedFiles no
